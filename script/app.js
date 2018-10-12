af780ab2581a193f82afc23ac5df2d146c34e11a
TheysKlaas <33479434+TheysKlaas@users.noreply.github.com>
2018-10-12 15:11:20 +0200

    toggle dropdown

--- script/app.js
+++ script/app.js
@@ -3,6 +3,8 @@ document.addEventListener( 'DOMContentLoaded', function() {
     console.log( '!Script loaded!' );
 
 handleFloatingLabel();
+handleQualityRange(); 
+handleDropdownButton();
 });
 
 function handleFloatingLabel (){
@@ -31,4 +33,34 @@ function handlePasswordSwitcher(){
     } else {
         passwordInput.type = "password";
     }
+}
+
+function handleQualityRange (){
+
+    var qualityInput = document.querySelector(".js-quality-input");
+    var inputValue = qualityInput.value;
+    var rangeHolder = document.querySelector(".js-range-holder");
+
+    //console.log("input: " + qualityInput + ", inputvalue" + inputValue + ", rangeholder: " + rangeHolder);
+
+    qualityInput.addEventListener('input',function(e){
+        rangeHolder.style.marginLeft = e.target.value + "%";
+        console.log(e.target);
+        rangeHolder.setAttribute("data-value", e.target.value);
+        //rangeHolder.innerHTML = (e.target.value);
+    })
+
+}
+function handleDropdownButton (){
+
+    let button = document.querySelector(".js-button-dd")
+    let buttonlist = document.querySelector(".js-dropdown-list")
+
+    button.addEventListener("click",function(event){
+        event.preventDefault();
+        // buttonlist.style.display = "block"
+        buttonlist.classList.toggle('c-dropdown-list__open');
+        
+    })
+    
 }
